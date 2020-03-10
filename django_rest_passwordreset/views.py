7a072e836f6e5a96a93884c432814c8362345708
Muhammadiyev <muxammadiyevmansur1996@gmail.com>
2020-03-10 09:39:14 +0500

    update password reset

--- django_rest_passwordreset/views.py
+++ django_rest_passwordreset/views.py
@@ -77,7 +77,7 @@ class ResetPasswordConfirm(GenericAPIView):
         password_reset_token_validation_time = get_password_reset_token_expiry_time()
 
         reset_password_token = ResetPasswordToken.objects.filter(
-            key=token).first()
+            key=token).last()
 
         if reset_password_token is None:
             return Response({'status': 'notfound'}, status=status.HTTP_404_NOT_FOUND)
@@ -89,25 +89,10 @@ class ResetPasswordConfirm(GenericAPIView):
             reset_password_token.delete()
             return Response({'status': 'expired'}, status=status.HTTP_404_NOT_FOUND)
 
-        if reset_password_token.user.eligible_for_reset():
-            pre_password_reset.send(
-                sender=self.__class__, user=reset_password_token.user)
-            try:
-                validate_password(
-                    password,
-                    user=reset_password_token.user,
-                    password_validators=get_password_validators(
-                        settings.AUTH_PASSWORD_VALIDATORS)
-                )
-            except ValidationError as e:
-                raise exceptions.ValidationError({
-                    'password': e.messages
-                })
-
-            reset_password_token.user.set_password(password)
-            reset_password_token.user.save()
-            post_password_reset.send(
-                sender=self.__class__, user=reset_password_token.user)
+        reset_password_token.user.set_password(password)
+        reset_password_token.user.save()
+        post_password_reset.send(
+            sender=self.__class__, user=reset_password_token.user)
 
         ResetPasswordToken.objects.filter(
             user=reset_password_token.user).delete()
