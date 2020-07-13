196ab220a454d14d3e156d3597ff336348979c3c
Muhammadiyev <muxammadiyevmansur1996@gmail.com>
2020-07-13 19:25:51 +0500

    update .gitignore

--- ost_api/settings.py
+++ ost_api/settings.py
@@ -12,7 +12,7 @@ from variables import (
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY','sdd')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
