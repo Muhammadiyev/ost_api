9882ea5f13b4bdb01d6b61ed7936c3c355911819
Muhammadiyev <muxammadiyevmansur1996@gmail.com>
2020-08-17 16:01:16 +0500

    updated

--- users/managers.py
+++ users/managers.py
@@ -28,7 +28,7 @@ class UserManager(BaseUserManager):
     
 
     def create_user(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
+        #extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(username, email, password, **extra_fields)
 
