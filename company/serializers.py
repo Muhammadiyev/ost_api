78a18808208471ca7359c6904423022cad689a8c
Muhammadiyev <muxammadiyevmansur1996@gmail.com>
2020-09-10 22:40:36 +0500

    updated

--- company/serializers.py
+++ company/serializers.py
@@ -37,7 +37,7 @@ class DepartmentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Department
-        fields = ['id', 'department_name_uz','department_name_ru', 'user']
+        fields = ['id', 'department_name_uz','department_name_ru', 'user','parent']
 
 
 class DepartmentOfUserSerializer(serializers.ModelSerializer):
