856263dad0cf6fff296bd32b3ae319400adc6730
Muhammadiyev <muxammadiyevmansur1996@gmail.com>
2020-10-08 22:14:53 +0500

    add OneToOneConf admin py

--- conference/admin.py
+++ conference/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Conference, ConferenceUser, TypeConf
+from .models import (
+    Conference, 
+    ConferenceUser, 
+    TypeConf, 
+    OneToOneConf
+)
 
 admin.site.register(Conference)
 admin.site.register(ConferenceUser)
 admin.site.register(TypeConf)
+admin.site.register(OneToOneConf)
