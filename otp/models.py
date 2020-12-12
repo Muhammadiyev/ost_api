9b5cb70d340ea63b840ca73a5a0da3ca844858c3
Muhammadiyev <muxammadiyevmansur1996@gmail.com>
2020-12-12 15:35:31 +0500

    updated serializers

--- otp/models.py
+++ otp/models.py
@@ -9,10 +9,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.timezone import now
 from company.models import Department
 from django.db.models import Q
+from django.contrib.auth import get_user_model
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.conf import settings
-
+User = get_user_model()
 def otp_expiry_time():
     return getattr(settings, 'DJANGO_REST_MULTITOKENAUTH_RESET_TOKEN_EXPIRY_TIME', 24)
 
@@ -34,3 +35,24 @@ class PhoneOTP(models.Model):
     
     def __str__(self):
         return str(self.phone) + 'is sent ' + str(self.otp)
+
+
+
+class ConfValidateKey(models.Model):
+    conference = models.ForeignKey(
+        'conference.Conference', on_delete=models.CASCADE, related_name="confvalidated_of_conference")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="confvalidated_of_user")
+    security_room = models.CharField(max_length=9, blank=True, null=True)
+    validated = models.BooleanField(
+        default=False, help_text='If it is true, that means user have validate otp correctly in second API')
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        verbose_name=_("When was this token generated")
+    )
+    count = models.IntegerField(default=1, help_text='Number of Limit')
+    
+    def __str__(self):
+        return str(self.user) + 'is sent ' + str(self.security_room)
